9842212f2c5ffbc3e19f2d851a90865d90da7bfb
gamja0624 <gracewiz213@gmail.com>
2024-07-01 12:15:25 +0900

    example 폴더 삭제, Items코드 정리

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,23 +1,20 @@
 import NavBar from "./NavBar/NavBar";
 import LandingPage from "./Landingpage/LandingPage";
-import Items from "./items/items"
 import Footer from "./Footer/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Detail from "./Detail/Detail";
-import ExTable from "./example/ExTable";
+import Items from "./Items/Items";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <div style={{ minHeight: "100vh" }}>
-        {/* 요청 경로로 페이지 이동: 특정 컴포넌트 실행  */}
         <Router>
           <Routes>
             <Route path="/" element={<LandingPage />} />
+            <Route path="/Items" element={<Items />} />
             <Route path="/movie/:movieId" element={<Detail />} />
-            <Route path="/items" element={<Items />} />
-            <Route path="/example/table" element={<ExTable />} />
           </Routes>
         </Router>
       </div>
--- client/src/components/items/items.js
+++ client/src/components/items/items.js
@@ -1,31 +1,8 @@
 import React from 'react'
 
-const Test = (props) => {
-  console.log(props.hello)
-  return <div>Test Component {props.hello}</div >
-}
-
-function Items() {
+const Items = () => {
   return (
-    <div>
-      <div>아이템이다</div>
-      <div />
-      <div></div>
-      <div>{false}</div>
-      <div>{null}</div>
-      <div>{undefined}</div>
-      <div>{true}</div>
-
-      {/*-------- [ 조건부 렌더링 ] ---------*/}
-      {true && '하이'}
-      {true && true}
-      {true && <Test hello='쉬고싶다' />}
-      {false && <Test />}
-      {<Test /> && false}
-      {true && 0}
-      {0 && true}
-
-    </div>
+    <div>상품정보</div>
   )
 }
 
